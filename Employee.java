3d01a2c9cfbe2d5b01e730b0a804e4aa0e33529b
SWE - Wid Seraj <120143921+wid21@users.noreply.github.com>
2023-05-09 00:12:06 +0300

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,44 @@
+public class Employee {
+
+    private String id ;
+    private String name ;
+    private int salary;
+
+    public Employee(){
+
+    }
+
+    public Employee(String id , String name, int salary){
+        this.id=id;
+        this.name=name;
+        this.salary=salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getAnualSalary(){
+        salary *= 12 ;
+        return salary;
+    }
+
+    public int raisedSalary(){
+        int increment =0;
+        increment +=(salary* 10 )/100;  // increase by 10 %
+        return salary = salary + increment;
+    }
+    public int getSalary() {
+        return salary;
+    }
+    @Override
+    public String toString() {
+        return "Employee " +
+                "Id ='" + id + '\'' +
+                ", name ='" + name + '\'' +
+                ", salary = " + salary ;
+    }
+}
